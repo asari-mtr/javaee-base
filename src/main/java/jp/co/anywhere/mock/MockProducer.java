3805c3a9549b78cee9be3e5ff624d001bcc876e6
Asari Mitsuteru <asari.mtr@gmail.com>
2015-11-03 21:47:04 +0900

    TODOのモック

--- src/main/java/jp/co/anywhere/mock/MockProducer.java
+++ src/main/java/jp/co/anywhere/mock/MockProducer.java
@@ -1,7 +1,28 @@
 package jp.co.anywhere.mock;
 
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Created by asari on 2015/11/03.
  */
-public class MockProducer {
+@Dependent
+public class MockProducer implements Serializable{
+  /**
+   *
+   * @return
+   */
+  @Produces
+  @Named("emptyCollection")
+  public Collection<String> produceEmptyCollection() {
+    Collection<String> list = new ArrayList<String>();
+    for(int i = 0; i < 10; i++) {
+      list.add("Hello " + i);
+    }
+    return list;
+  }
 }
