ac9b83204c7d0e81d8dbdebdcc31f0c1ed089b00
Asari Mitsuteru <asari.mtr@gmail.com>
2015-11-07 17:08:17 +0900

    Entityを追加、自動で生成できるようにした

---
+++ src/main/java/jp/co/anywhere/entity/AbstractEntity.java
@@ -0,0 +1,33 @@
+package jp.co.anywhere.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by asari on 2015/11/07.
+ */
+@MappedSuperclass
+public abstract class AbstractEntity {
+  @Id
+  @Column
+  @GeneratedValue
+  private Long id;
+
+  @Version
+  private long version;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public long getVersion() {
+    return version;
+  }
+
+  public void setVersion(long version) {
+    this.version = version;
+  }
+}
---
+++ src/main/java/jp/co/anywhere/entity/TaskItem.java
@@ -0,0 +1,34 @@
+package jp.co.anywhere.entity;
+
+import jp.co.anywhere.entity.AbstractEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ * Created by asari on 2015/11/07.
+ */
+@Entity
+public class TaskItem extends AbstractEntity {
+
+  @Column
+  private String task;
+  @Column
+  private boolean done = false;
+
+  public String getTask() {
+    return task;
+  }
+
+  public void setTask(String task) {
+    this.task = task;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+}
