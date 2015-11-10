6f090c4a7c029cf7cc0db4b2fd512f42d39ce8ad
Asari Mitsuteru <asari.mtr@gmail.com>
2015-11-11 00:26:40 +0900

    作成日、更新日をEntityに追加。保存時更新時に自動で日時が更新されるように。

--- src/main/java/jp/co/anywhere/entity/AbstractEntity.java
+++ src/main/java/jp/co/anywhere/entity/AbstractEntity.java
@@ -2,19 +2,49 @@ package jp.co.anywhere.entity;
 
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by asari on 2015/11/07.
  */
 @MappedSuperclass
 public abstract class AbstractEntity implements Entity {
+  @Transient
+  private boolean persist;
   @Id
   @Column
   @GeneratedValue
   private Long id;
-
   @Version
   private long version;
+  @Column
+  private Date createDate;
+  @Column
+  private Date updateDate;
+
+  @PostPersist
+  @PostUpdate
+  @PostLoad
+  private void postPersist() {
+    persist = true;
+  }
+  @PrePersist
+  private void prePersist() {
+    createDate = new Date();
+    updateDate = new Date();
+  }
+  @PreUpdate
+  private void preUpdate() {
+    if(createDate == null) {
+      createDate = new Date();
+    }
+    updateDate = new Date();
+  }
+
+  @Override
+  public boolean isPersist() {
+    return persist;
+  }
 
   @Override
   public Long getId() {
@@ -34,5 +64,23 @@ public abstract class AbstractEntity implements Entity {
   @Override
   public void setVersion(long version) {
     this.version = version;
+  }
+
+  @Override
+  public Date getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(Date createDate) {
+    this.createDate = createDate;
+  }
+
+  @Override
+  public Date getUpdateDate() {
+    return updateDate;
+  }
+
+  public void setUpdateDate(Date updateDate) {
+    this.updateDate = updateDate;
   }
 }
--- src/main/java/jp/co/anywhere/entity/Entity.java
+++ src/main/java/jp/co/anywhere/entity/Entity.java
@@ -1,11 +1,14 @@
 package jp.co.anywhere.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by asari on 2015/11/07.
  */
 public interface Entity extends Serializable {
+
+  boolean isPersist();
 
   Long getId();
 
@@ -14,4 +17,8 @@ public interface Entity extends Serializable {
   long getVersion();
 
   void setVersion(long version);
+
+  Date getCreateDate();
+
+  Date getUpdateDate();
 }
