c866cd25ca5cfe4eed1dc8cbcfa429a1897a518c
Asari Mitsuteru <asari.mtr@gmail.com>
2015-11-21 20:21:47 +0900

    文字列用のヘルパーを追加

---
+++ src/main/java/jp/co/anywhere/consumer/shared/util/StringHelper.java
@@ -0,0 +1,14 @@
+package jp.co.anywhere.consumer.shared.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by asari on 2015/11/21.
+ */
+public class StringHelper {
+
+  public static boolean isEmpty(CharSequence value) {
+    return StringUtils.isEmpty(value);
+  }
+
+}
