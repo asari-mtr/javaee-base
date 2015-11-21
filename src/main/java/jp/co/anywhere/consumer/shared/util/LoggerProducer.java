24be9cd9787f496c6119198143c600c6cbeef63a
Asari Mitsuteru <asari.mtr@gmail.com>
2015-11-21 22:49:12 +0900

    LoggerProducerを追加

---
+++ src/main/java/jp/co/anywhere/consumer/shared/util/LoggerProducer.java
@@ -0,0 +1,20 @@
+package jp.co.anywhere.consumer.shared.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+/**
+ * Created by asari on 2015/11/21.
+ */
+@Dependent
+public class LoggerProducer {
+
+  @Produces
+  public Logger getLogger(InjectionPoint point) {
+    return LoggerFactory.getLogger(point.getMember().getDeclaringClass());
+  }
+}
